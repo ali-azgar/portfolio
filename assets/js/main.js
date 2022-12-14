93a752d4b62e209d4b439787fdcd838845b517a4
ali-azgar <66671479+ali-azgar@users.noreply.github.com>
2022-12-14 11:17:07 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,5 @@
-/**
-* Template Name: PhotoFolio - v1.1.1
-* Template URL: https://bootstrapmade.com/photofolio-bootstrap-photography-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
-
-  /**
-   * Preloader
-   */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      setTimeout(() => {
-        preloader.classList.add('loaded');
-      }, 1000);
-      setTimeout(() => {
-        preloader.remove();
-      }, 2000);
-    });
-  }
 
   /**
    * Mobile nav toggle
